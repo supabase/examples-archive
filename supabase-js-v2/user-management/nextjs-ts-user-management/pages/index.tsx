36531d3dc384d96bcd712ab35ef8b23ce6a650cc
Francisco Mazzoni <fran.mazzoni@gmail.com>
2022-08-12 16:21:26 -0300

    fix type for getPublicProfiles

--- supabase-js-v2/user-management/nextjs-ts-user-management/pages/index.tsx
+++ supabase-js-v2/user-management/nextjs-ts-user-management/pages/index.tsx
@@ -1,58 +1,62 @@
-import Auth from '../components/Auth'
-import Account from '../components/Account'
-import ProfileList from '../components/ProfileList'
-import { useState, useEffect } from 'react'
-import { supabase } from '../lib/supabaseClient'
-import { AuthSession } from '@supabase/supabase-js'
-import { Profile } from '../lib/constants'
-import Footer from '../components/Footer'
+import Auth from "../components/Auth";
+import Account from "../components/Account";
+import ProfileList from "../components/ProfileList";
+import { useState, useEffect } from "react";
+import { supabase } from "../lib/supabaseClient";
+import { AuthSession } from "@supabase/supabase-js";
+import { Profile } from "../lib/constants";
+import Footer from "../components/Footer";
 
 export default function Home() {
-  const [session, setSession] = useState<AuthSession | null>(null)
-  const [profiles, setProfiles] = useState<Profile[]>([])
+  const [session, setSession] = useState<AuthSession | null>(null);
+  const [profiles, setProfiles] = useState<Profile[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await supabase.auth.getSession()
-      return data
-    }
+      const data = await supabase.auth.getSession();
+      return data;
+    };
 
     fetchData()
       .then(({ session }) => {
-        setSession(session)
+        setSession(session);
       })
       .catch((error) => {
-        console.log(error)
-      })
+        console.log(error);
+      });
 
-    supabase.auth.onAuthStateChange((_event: string, session: AuthSession | null) => {
-      setSession(session)
-    })
-  }, [session])
+    supabase.auth.onAuthStateChange(
+      (_event: string, session: AuthSession | null) => {
+        setSession(session);
+      }
+    );
+  }, [session]);
 
   useEffect(() => {
-    getPublicProfiles()
-  }, [])
+    getPublicProfiles();
+  }, []);
 
   async function getPublicProfiles() {
     try {
       const { data, error } = await supabase
-        .from<Profile>('profiles')
-        .select('id, username, avatar_url, website, updated_at')
-        .order('updated_at', { ascending: false })
+        .from("profiles")
+        .select("id, username, avatar_url, website, updated_at")
+        .order("updated_at", { ascending: false });
 
       if (error || !data) {
-        throw error || new Error('No data')
+        throw error || new Error("No data");
       }
-      console.log('Public profiles:', data)
-      setProfiles(data)
+      console.log("Public profiles:", data);
+      setProfiles(data);
     } catch (error) {
-      console.log('error', error.message)
+      if (error instanceof Error) {
+        console.log("error", error.message);
+      }
     }
   }
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
       {!session ? (
         <Auth />
       ) : (
@@ -70,5 +74,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
