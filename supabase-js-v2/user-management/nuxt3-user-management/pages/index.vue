d4757e089300e38838ced855cc96db4bbb485161
Andrew Smith <a.smith@silentworks.co.uk>
2022-08-29 13:38:22 +0000

    V2/user management nuxt3 update (#31)
    * Initial commit
    
    * Update to work with supabase v2

---
+++ supabase-js-v2/user-management/nuxt3-user-management/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container" style="padding: 50px 0 100px 0">
+        <Account v-if="user" />
+        <Auth v-else />
+    </div>
+</template>
+
+<script setup>
+const user = useUser();
+user.value = useSupabaseUser()
+</script>
